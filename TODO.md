0529e8122dfb0b912543494418bdd5b5012ac981
Jia-Shen Boon <jiashen@gmail.com>
2021-08-14 07:24:25 +0900

    Add record for blog
    Updated version of cdk-core because it wasn't compatible with the record TTL property. `npm install @aws-sdk/core@1.118.0`
    
    __Testing done__
    
    1. `cdk deploy`
    1. See in Route53 console that new record is created with TTL of 60 seconds
    1. <http://blog.boonjiashen-dev.com> still doesn't route to blog, so I'll need to check why. That's tracked in the TODO.

--- TODO.md
+++ TODO.md
@@ -1,1 +1,3 @@
-* Route blog.* to blog
+* Check that blog.boonjiashen-dev.com routes to blog
+* Add formatter (gts)
+* Remove dev domain from search engine crawls
--- lib/boonjiashen-dot-com-infra-stack.ts
+++ lib/boonjiashen-dot-com-infra-stack.ts
@@ -1,12 +1,20 @@
 import * as cdk from '@aws-cdk/core';
 import * as route53 from '@aws-cdk/aws-route53';
+import { Duration } from '@aws-cdk/core';
 
 export class BoonjiashenDotComInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new route53.HostedZone(this, "boonjiashen-dev-dot-com", {
+    const zone = new route53.HostedZone(this, "boonjiashen-dev-dot-com", {
       zoneName: "boonjiashen-dev.com",
+    });
+
+    const blogRecord = new route53.CnameRecord(this, "blogRecord", {
+      zone: zone,
+      domainName: "boonjiashen.github.io",
+      recordName: "blog." + zone.zoneName,
+      ttl: Duration.seconds(60),
     })
   }
 }
