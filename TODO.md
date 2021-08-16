789d3070b04760480cfed48a05b3dd127d900c86
Jia-Shen Boon <jiashen@gmail.com>
2021-08-17 07:11:33 +0900

    Add documentation for hosted zone, Github
    Previously the subdomain wasn't directing to the blog page because
    1. I didn't provide the auto-created name servers to the domain registrar
    2. Each Github username only allows for one subdomain
    
    I used a separate Github username for the dev endpoint, and updated the name servers with the domain registrar.
    
    __Testing done__
    
    1. `cdk deploy`
    1. <http://blog.boonjiashen-dev.com/> points to the blog

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
-* Check that blog.boonjiashen-dev.com routes to blog
 * Add formatter (gts)
 * Remove dev domain from search engine crawls
--- lib/boonjiashen-dot-com-infra-stack.ts
+++ lib/boonjiashen-dot-com-infra-stack.ts
@@ -6,13 +6,22 @@ export class BoonjiashenDotComInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const topLevelDomainName = "boonjiashen-dev.com";
+
+    // This zone will include automatically created name servers, which you will need to
+    // be provided to the domain registrar, e.g.,
+    // https://console.aws.amazon.com/route53/home#DomainDetail:boonjiashen-dev.com
     const zone = new route53.HostedZone(this, "boonjiashen-dev-dot-com", {
-      zoneName: "boonjiashen-dev.com",
+      zoneName: topLevelDomainName,
     });
 
+    // This GitHub repository needs to be associated with `recordName` in the Github Pages tab
+    // e.g., https://github.com/boonjiashen2/boonjiashen2.github.io/settings/pages which updates a CNAME file
+    // in the repository
+    const blogDomainName = "boonjiashen2.github.io";
     const blogRecord = new route53.CnameRecord(this, "blogRecord", {
       zone: zone,
-      domainName: "boonjiashen.github.io",
+      domainName: blogDomainName,
       recordName: "blog." + zone.zoneName,
       ttl: Duration.seconds(60),
     })
