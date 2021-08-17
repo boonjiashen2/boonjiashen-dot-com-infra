36e6c74f594388beadb1a59509093ea241f9b622
Jia-Shen Boon <jiashen@gmail.com>
2021-08-18 06:42:53 +0900

    Add autolinter

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+# Fix formatting
+
+Run `npm run lint` to run linter
+
+Run `npm run gix` to fix formatting/linting problems, if possible.
+
+
+# Deploy
+
+`cdk deploy`
--- test/boonjiashen-dot-com-infra.test.ts
+++ test/boonjiashen-dot-com-infra.test.ts
@@ -1,13 +1,21 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {expect as expectCDK, matchTemplate, MatchStyle} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as BoonjiashenDotComInfra from '../lib/boonjiashen-dot-com-infra-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new BoonjiashenDotComInfra.BoonjiashenDotComInfraStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new BoonjiashenDotComInfra.BoonjiashenDotComInfraStack(
+    app,
+    'MyTestStack'
+  );
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
